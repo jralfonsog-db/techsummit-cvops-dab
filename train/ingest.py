2f98ad01087b5e35e2815eb8d65af1a18c98edce
jralfonsog-db <jralfonsog-db>
2023-08-25 13:02:44 +0000

    ingest.py wip

--- train/ingest.py
+++ train/ingest.py
@@ -7,14 +7,27 @@ from pyspark.sql.functions import regexp_replace
 
 @dlt.table
 @dlt.expect("No null links", "link is not null")
-def medium_raw():
+def pcb_images():
     csv_path = "dbfs:/data-asset-bundles-dais2023/fe_medium_posts_raw.csv"
     return spark.read.csv(csv_path, header=True)
-
-
+  
 @dlt.table
-def medium_clean():
-    df: DataFrame = dlt.read("medium_raw")
-    df = df.filter(df.link != 'null')
-    df = df.withColumn("author", regexp_replace("author", "\\([^()]*\\)", ""))
+@dlt.expect("No null links", "link is not null")
+def pcb_labels():
+    csv_path = "dbfs:/data-asset-bundles-dais2023/fe_medium_posts_raw.csv"
+    return spark.read.csv(csv_path, header=True)
+  
+@dlt.table
+def training_dataset():
+    pcb_images = dlt.read("pcb_images")
+    pcb_labels = dlt.read("pcb_labels")
+    df = pcb_images
     return df
+  
+def training_dataset_augmented():
+  df = None
+  return df
+
+def delta_torch_dataset():
+  df = None
+  return df
