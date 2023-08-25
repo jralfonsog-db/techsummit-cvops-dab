60514f64a36093d6ea54b7c06a824d14bffffb63
jose.alfonso@databricks.com <jose.alfonso@databricks.com>
2023-08-25 18:49:47 +0200

    Changed project structure

--- etl/ingest.py
+++ etl/ingest.py
@@ -8,13 +8,13 @@ from pyspark.sql.functions import regexp_replace
 @dlt.table
 @dlt.expect("No null links", "link is not null")
 def pcb_images():
-    csv_path = "dbfs:/data-asset-bundles-dais2023/fe_medium_posts_raw.csv"
+    csv_path = ""
     return spark.read.csv(csv_path, header=True)
   
 @dlt.table
 @dlt.expect("No null links", "link is not null")
 def pcb_labels():
-    csv_path = "dbfs:/data-asset-bundles-dais2023/fe_medium_posts_raw.csv"
+    csv_path = ""
     return spark.read.csv(csv_path, header=True)
   
 @dlt.table
--- typings/__builtins__.pyi
+++ typings/__builtins__.pyi
@@ -1,2 +1,1 @@
-
 from databricks.sdk.runtime import *
