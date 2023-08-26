393086912178442e0a3375d4444b3f83f566bfdb
jralfonsog-db <jralfonsog-db>
2023-08-26 07:30:00 +0000

    ingest.py now works properly

--- etl/ingest.py
+++ etl/ingest.py
@@ -2,32 +2,38 @@ from os import path
 
 import dlt
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import regexp_replace
+from pyspark.sql.functions import regexp_replace, col
+import pyspark.sql.functions as F
 
+# catalog = spark.sql("SELECT current_catalog()").collect()[0][0]
+# schema = spark.sql("SELECT current_schema()").collect()[0][0]
+
+catalog = "cvops-test"
+schema = "pcb"
 
 @dlt.table
-@dlt.expect("No null links", "link is not null")
+@dlt.expect("No null images", "content is not null")
 def pcb_images():
-    csv_path = ""
-    return spark.read.csv(csv_path, header=True)
-  
-@dlt.table
-@dlt.expect("No null links", "link is not null")
-def pcb_labels():
-    csv_path = ""
-    return spark.read.csv(csv_path, header=True)
-  
-@dlt.table
-def training_dataset():
-    pcb_images = dlt.read("pcb_images")
-    pcb_labels = dlt.read("pcb_labels")
-    df = pcb_images
-    return df
-  
-def training_dataset_augmented():
-  df = None
-  return df
+    pcb_images_path = f"/Volumes/{catalog}/{schema}/landing/Images/"
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "binaryFile")
+        .option("pathGlobFilter", "*.JPG")
+        .option("recursiveFileLookup", "true")
+        .load(pcb_images_path)
+        .withColumn("filename", F.substring_index(col("path"), "/", -1))
+    )
 
-def delta_torch_dataset():
-  df = None
-  return df
+@dlt.table
+@dlt.expect("No null labels", "labelDetail is not null")
+def pcb_labels():
+    csv_path = f"/Volumes/{catalog}/{schema}/landing/labels/"
+    return (
+        spark.readStream.format("cloudFiles")
+        .option("cloudFiles.format", "csv")
+        .option("header", True)
+        .load(f"/Volumes/cvops/pcb/landing/labels/")
+        .withColumn("filename", F.substring_index(col("image"), "/", -1))
+        .select("filename", "label")
+        .withColumnRenamed("label", "labelDetail")
+    )
