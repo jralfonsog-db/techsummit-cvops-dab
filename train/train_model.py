61174d193e572a7c946437bda46859253a5b83fc
jralfonsog-db <jralfonsog-db>
2023-09-08 09:06:02 +0000

    train_model (WIP)

--- train/train_model.py
+++ train/train_model.py
@@ -9,28 +9,35 @@ from deltatorch import create_pytorch_dataloader
 from deltatorch import FieldSpec
 import torchvision.transforms as tf
 import io
+from io import BytesIO
 import os
 import torch
 from torchvision import models
 from torchmetrics import Accuracy
 from torch.nn import functional as nnf
 from pytorch_lightning.loggers import MLFlowLogger
+import mlflow
 from mlflow.utils.file_utils import TempDir
 import cloudpickle
+import base64
+import pandas as pd
 
 # COMMAND ----------
 
 dbutils.widgets.text("environment", "environment")
 dbutils.widgets.text("catalog", "catalog")
 dbutils.widgets.text("schema", "schema")
+dbutils.widgets.text("model_name", "model_name")
 
 environment = dbutils.widgets.get("environment")
 catalog = dbutils.widgets.get("catalog")
 schema = dbutils.widgets.get("schema")
+model_name = dbutils.widgets.get("model_name")
 
 # COMMAND ----------
 
 ckpt_path = f"/Volumes/{catalog}/{schema}/torch_checkpoints/"
+training_dataset_augmented_table = "training_dataset_augmented"
 train_deltatorch_path = f"/Volumes/{catalog}/{schema}/deltatorch_files/train"
 test_deltatorch_path = f"/Volumes/{catalog}/{schema}/deltatorch_files/test"
 
@@ -39,7 +46,8 @@ test_deltatorch_path = f"/Volumes/{catalog}/{schema}/deltatorch_files/test"
 MAX_EPOCH_COUNT = 20
 BATCH_SIZE = 16
 STEPS_PER_EPOCH = 2
-EARLY_STOP_MIN_DELTA = 0.01
+# EARLY_STOP_MIN_DELTA = 0.01
+EARLY_STOP_MIN_DELTA = 0.5
 EARLY_STOP_PATIENCE = 10
 NUM_WORKERS = 8
 
@@ -60,6 +68,83 @@ spark.sql(f"USE SCHEMA {schema}")
 
 # COMMAND ----------
 
+# Below are initialization related functions
+def get_cloud_name():
+    return spark.conf.get("spark.databricks.clusterUsageTags.cloudProvider").lower()
+
+
+def get_current_url():
+    return dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get()
+
+
+def get_username() -> str:  # Get the user's username
+    return (
+        dbutils()
+        .notebook.entry_point.getDbutils()
+        .notebook()
+        .getContext()
+        .tags()
+        .apply("user")
+        .lower()
+        .split("@")[0]
+        .replace(".", "_")
+    )
+
+
+cleaned_username = get_username()
+
+
+def get_pat():
+    return (
+        dbutils.notebook.entry_point.getDbutils()
+        .notebook()
+        .getContext()
+        .apiToken()
+        .get()
+    )
+
+
+def get_request_headers() -> str:
+    return {
+        "Authorization": f"""Bearer {dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()}"""
+    }
+
+
+def get_instance() -> str:
+    return (
+        dbutils()
+        .notebook.entry_point.getDbutils()
+        .notebook()
+        .getContext()
+        .tags()
+        .apply("browserHostName")
+    )
+
+# COMMAND ----------
+
+print(get_cloud_name())
+print(get_current_url())
+print(get_username())
+print(get_request_headers())
+print(get_instance())
+
+# COMMAND ----------
+
+def set_experiment_permission(experiment_path):
+  url = dbutils.notebook.entry_point.getDbutils().notebook().getContext().extraContext().apply("api_url")
+  import requests
+  pat_token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+  headers =  {"Authorization": "Bearer " + pat_token, 'Content-type': 'application/json'}
+  status = requests.get(url+"/api/2.0/workspace/get-status", params = {"path": experiment_path}, headers=headers).json()
+  if "object_id" not in status:
+        print(f"error setting up shared experiment permission: {status}")
+  else:
+    #Set can manage to all users to the experiment we created as it's shared among all
+    params = {"access_control_list": [{"group_name": "users","permission_level": "CAN_MANAGE"}]}
+    permissions = requests.patch(f"{url}/api/2.0/permissions/experiments/{status['object_id']}", json = params, headers=headers)
+    if permissions.status_code != 200:
+      print("ERROR: couldn't set permission to all users to the autoML experiment")
+          
 def init_experiment_for_batch(demo_name, experiment_name):
   #You can programatically get a PAT token with the following
   pat_token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
@@ -67,11 +152,12 @@ def init_experiment_for_batch(demo_name, experiment_name):
   #current_user = dbutils.notebook.entry_point.getDbutils().notebook().getContext().tags().apply('user')
   import requests
   xp_root_path = f"/dbdemos/experiments/{demo_name}"
-  requests.post(f"{get_current_url()}/api/2.0/workspace/mkdirs", headers = get_request_headers(), json={ "path": xp_root_path})
-  xp_path = f"{xp_root_path}/{experiment_name}"
-  mlflow.set_experiment(xp_path)
-  xp = mlflow.get_experiment_by_name(xp_path)
-  set_experiment_permission(xp.experiment_id, xp.name)
+  requests.post(f"{url}/api/2.0/workspace/mkdirs", headers = {"Accept": "application/json", "Authorization": f"Bearer {pat_token}"}, json={ "path": xp_root_path})
+  xp = f"{xp_root_path}/{experiment_name}"
+  print(f"Using common experiment under {xp}")
+  mlflow.set_experiment(xp)
+  set_experiment_permission(xp)
+  return mlflow.get_experiment_by_name(xp)
 
 # COMMAND ----------
 
@@ -200,19 +286,53 @@ class CVModel(pl.LightningModule):
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC ## MLflow model wrapper
+# MAGIC
+# MAGIC This wrapper will override the default predict method to allow our model to include the required image preprocessing to receive images in base64 for inference
+
+# COMMAND ----------
+
+id2label = {0: "normal", 1: "anomaly"}
+def predict_byte_serie(content_as_byte_serie, model, device = torch.device("cpu")):
+  #Transform the bytes as PIL images
+  image_list = content_as_byte_serie.apply(lambda b: Image.open(io.BytesIO(b))).to_list()
+  #Apply our transformations & stack them for our model
+  vector_images = torch.stack([transform(b).to(device) for b in image_list])
+  #Call the model to get our inference
+  outputs = model(vector_images)
+  #Returns a proper results with Score/Label/Name
+  preds = torch.max(outputs, 1)[1].tolist()
+  probs = torch.nn.functional.softmax(outputs, dim=-1)[:, 1].tolist()
+  return pd.DataFrame({"score": probs, "label": preds, "labelName": [id2label[p] for p in preds]})
+
+# COMMAND ----------
+
+# Model wrapper
+class CVTorchModelWrapper(mlflow.pyfunc.PythonModel):
+    def __init__(self, model):
+        self.model = model
+        # instantiate model in evaluation mode
+        self.model.eval()
+
+    def predict(self, context, images):
+        with torch.set_grad_enabled(False):
+          #Convert the base64 to PIL images
+          images = images['data'].apply(lambda b: base64.b64decode(b))
+          return predict_byte_serie(images, self.model)
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ## Train function
 
 # COMMAND ----------
 
-db_host = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiUrl().get()
-db_token = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
-
-ckpt_path = f"/dbfs/dbdemos/cv_torch/checkpoints"
+xp = init_experiment_for_batch("techsummit-cvops", f"{environment}-experiment")
 
 def train_model(dm, num_gpus=1, single_node=True):
     # We put them into these environment variables as this is where mlflow will look by default
-    os.environ['DATABRICKS_HOST'] = db_host
-    os.environ['DATABRICKS_TOKEN'] = db_token
+    os.environ['DATABRICKS_HOST'] = get_current_url()
+    os.environ['DATABRICKS_TOKEN'] = get_pat()
     torch.set_float32_matmul_precision("medium")
     if single_node or num_gpus == 1:
         num_devices = num_gpus
@@ -251,8 +371,7 @@ def train_model(dm, num_gpus=1, single_node=True):
 
     # AutoLog does not work with DDP 
     mlflow.pytorch.autolog(disable=False, log_models=False)
-    with mlflow.start_run(run_name="torch") as run:
-      
+    with mlflow.start_run() as run:
       mlf_logger = MLFlowLogger(experiment_name=xp.name, run_id=run.info.run_id)
       # Initialize a trainer
       trainer = pl.Trainer(
@@ -267,9 +386,9 @@ def train_model(dm, num_gpus=1, single_node=True):
           logger=mlf_logger)
 
       print(f"Global Rank: {trainer.global_rank} - Local Rank: {trainer.local_rank} - World Size: {trainer.world_size}")
-      device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+      # device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
       model = CVModel(2)
-      model.to(device)
+      # model.to(device)
       
       trainer.fit(model, dm)
       print("Training is done ")
@@ -278,7 +397,9 @@ def train_model(dm, num_gpus=1, single_node=True):
       if trainer.global_rank == 0:
         print("Logging our model")
         reqs = mlflow.pytorch.get_default_pip_requirements() + ["pytorch-lightning==" + pl.__version__]
-        mlflow.pytorch.log_model(artifact_path="model", pytorch_model=model.model, pip_requirements=reqs)
+        # pytorch_model = CVTorchModelWrapper(model.model)  # We use our previously defined class to wrap our model including the overriden predict method
+        pytorch_model = model.model
+        mlflow.pytorch.log_model(artifact_path="model", pytorch_model=pytorch_model, pip_requirements=reqs)
         #Save the test/validate transform as we'll have to apply the same transformation in our pipeline.
         #We could alternatively build a model wrapper to encapsulate these transformations as part as of our model (that's what we did with the huggingface implementation).
         with TempDir(remove_on_exit=True) as local_artifacts_dir:
@@ -287,10 +408,11 @@ def train_model(dm, num_gpus=1, single_node=True):
           with open(transform_path, "wb") as fd:
             cloudpickle.dump(dm.transform, fd)
           mlflow.log_artifact(transform_path, "model")
-        mlflow.set_tag("dbdemos", "cpb_torch")
+        mlflow.set_tag("cvops", "pcb_torch")
         #log and returns model accuracy
         mlflow.log_metrics(val_metrics[0])
-        return run
+    
+    return mlf_logger.run_id
 
 # COMMAND ----------
 
@@ -304,6 +426,61 @@ def train_model(dm, num_gpus=1, single_node=True):
 delta_dataloader = DeltaDataModule(train_deltatorch_path, test_deltatorch_path)
 
 if environment == "development":
-  run = train_model(delta_dataloader, 1, True)
+  run_id = train_model(delta_dataloader, 1, True)
+elif environment == "preproduction" or "production":
+  run_id = train_model(delta_dataloader, 2, True)
 else:
-  run = train_model(delta_dataloader, 2, True)
+  print("Wrong environment. Please select 'development', 'preproduction' or 'production'.")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Register model in UC
+
+# COMMAND ----------
+
+mlflow.set_registry_uri("databricks-uc")
+mlflow.register_model(f"runs:/{run_id}/model", f"{catalog}.{schema}.{model_name}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Model Serving
+
+# COMMAND ----------
+
+from io import BytesIO
+import base64
+import pandas as pd
+
+# Model wrapper
+class RealtimeCVTorchModelWrapper(mlflow.pyfunc.PythonModel):
+    def __init__(self, model):
+        self.model = model
+        # instantiate model in evaluation mode
+        self.model.eval()
+
+    def predict(self, context, images):
+        with torch.set_grad_enabled(False):
+          #Convert the base64 to PIL images
+          images = images['data'].apply(lambda b: base64.b64decode(b))
+          return predict_byte_serie(images, self.model)
+
+
+#Load it as CPU as our endpoint will be cpu for now
+model_cpu = mlflow.pytorch.load_model(f"runs:/{run_id}/model").to(torch.device("cpu"))
+rt_model = RealtimeCVTorchModelWrapper(model_cpu)
+
+def to_base64(b):
+  return base64.b64encode(b).decode("ascii")
+
+
+df = spark.table(training_dataset_augmented_table)
+
+#Let's try locally before deploying our endpoint to make sure it works as expected:
+pdf = df.limit(10).toPandas()
+
+#Transform our input as a pandas dataframe containing base64 as this is what our serverless model endpoint will receive.
+input_example = pd.DataFrame(pdf["content"].apply(to_base64).to_list(), columns=["data"])
+predictions = rt_model.predict(None, input_example)
+display(predictions)
